4d392ff658f835dad98d86b4ae74ba071fa38cc1
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-19 16:00:57 +0100

    in progress

--- src/main/scala/SimpleLinkedList.scala
+++ src/main/scala/SimpleLinkedList.scala
@@ -1,8 +1,19 @@
-trait SimpleLinkedList[T] {
-  def isEmpty: Boolean
-  def value: T
-  def add(item: T): SimpleLinkedList[T]
-  def next: SimpleLinkedList[T]
-  def reverse: SimpleLinkedList[T]
-  def toSeq: Seq[T]
-}
+trait SimpleLinkedList[T]:
+   def isEmpty: Boolean
+   def value: T
+   def add(item: T): SimpleLinkedList[T]
+   def next: SimpleLinkedList[T]
+   def reverse: SimpleLinkedList[T]
+   def toSeq: Seq[T]
+
+case object Empty extends SimpleLinkedList:
+   
+case class NonEmpty[T]() extends SimpleLinkedList:
+
+object SimpleLinkedList:
+
+   def fromSeq[T](ts: Seq[T]): List[T] = ???
+
+   def apply[T](): SimpleLinkedList[T] = ???
+
+   def empty[T]: SimpleLinkedList[T] = ???
