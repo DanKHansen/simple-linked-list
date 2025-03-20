046dd34348b55823d9b462f9d22fdd448e84421b
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-20 10:40:16 +0100

    solution

--- src/main/scala/SimpleLinkedList.scala
+++ src/main/scala/SimpleLinkedList.scala
@@ -6,14 +6,14 @@ trait SimpleLinkedList[T]:
    def reverse: SimpleLinkedList[T]
    def toSeq: Seq[T]
 
-case object Empty extends SimpleLinkedList:
-   
-case class NonEmpty[T]() extends SimpleLinkedList:
-
 object SimpleLinkedList:
+   def fromSeq[T](seq: Seq[T]): SimpleLinkedList[T] = LinkedList(seq.toList)
+   def apply[T](ts: T*): SimpleLinkedList[T] = fromSeq[T](ts.toSeq)
 
-   def fromSeq[T](ts: Seq[T]): List[T] = ???
-
-   def apply[T](): SimpleLinkedList[T] = ???
-
-   def empty[T]: SimpleLinkedList[T] = ???
+class LinkedList[T](l: List[T]) extends SimpleLinkedList[T]:
+   override def isEmpty: Boolean = l.isEmpty
+   override def value: T = l.head
+   override def add(item: T): SimpleLinkedList[T] = LinkedList(l.appended(item))
+   override def next: SimpleLinkedList[T] = LinkedList(l.tail)
+   override def reverse: SimpleLinkedList[T] = LinkedList(l.reverse)
+   override def toSeq: Seq[T] = l
